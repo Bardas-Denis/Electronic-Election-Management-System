ffc65de4ad8a0a578d58404c90e578a947710672
TeodorFilea <teodorfilea13@gmail.com>
2026-08-11 12:06:06 +0300

    Update

--- electronic-election-management-system/src/main.ts
+++ electronic-election-management-system/src/main.ts
@@ -1,3 +1,4 @@
+// Bootstraps the root component with global providers
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
